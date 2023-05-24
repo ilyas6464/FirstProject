b4865d77ec9137bb9830bdf1d1cfc4e440dc764e
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-05-24 10:32:20 -0400

    may 24

---
+++ force-app/main/default/lwc/practice4/practice4.js
@@ -0,0 +1,36 @@
+import { LightningElement } from 'lwc';
+
+export default class Practice4 extends LightningElement {
+
+    num1 = 0 ; 
+    num2 = 0;
+    
+    get additionResult() {
+        return this.num1 + this.num2; 
+    }
+
+    get multiplicationResult() {
+        return this.num1 * this.num2; 
+    }
+
+    get substractionResult() {
+        return this.num1 - this.num2; 
+    }
+    get divisionResult() {
+        return this.num1 / this.num2; 
+    }
+
+
+
+    handleNum1Change(event) {
+        // set the value of num1 to what user typed
+        this.num1 = parseInt( event.target.value) ; 
+    }
+
+    handleNum2Change(event) {
+        // set the value of num2 to what user typed
+        this.num2 = parseInt( event.target.value); 
+    }
+
+
+}
