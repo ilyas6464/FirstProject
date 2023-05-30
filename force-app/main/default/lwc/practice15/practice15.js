745776a0bab12199daa132272beee212fd7b57fe
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-05-30 14:41:59 -0400

    may30

---
+++ force-app/main/default/lwc/practice15/practice15.js
@@ -0,0 +1,9 @@
+import { LightningElement } from "lwc";
+import getAccountWithAnnualRevenue from "@salesforce/apex/AccountController.getAccountWithAnnualRevenue";
+
+export default class Practice15 extends LightningElement {
+
+    
+    wiredAccount;
+
+}
