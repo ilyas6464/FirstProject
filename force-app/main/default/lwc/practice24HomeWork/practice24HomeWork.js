221247e208cbaa7635984e1a4ea6e9a8a6a81641
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-06-05 13:07:57 -0400

    june 5

--- force-app/main/default/lwc/practice24HomeWork/practice24HomeWork.js
+++ force-app/main/default/lwc/practice24HomeWork/practice24HomeWork.js
@@ -4,31 +4,20 @@ import NAME_FIELD from "@salesforce/schema/Opportunity.Name";
 import STAGE_FIELD from "@salesforce/schema/Opportunity.StageName";
 import CLOSE_DATE_FIELD from "@salesforce/schema/Opportunity.CloseDate";
 import AMOUNT_FIELD from "@salesforce/schema/Opportunity.Amount";
-
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
-
 export default class Practice24 extends LightningElement {
+    objectApiName = OPP_OBJECT;
 
-    objectApiName = OPP_OBJECT; 
+    fields=[NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD, AMOUNT_FIELD];
 
-    fields = [NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD, AMOUNT_FIELD]; 
-
-
-    showSuccess(event) {
-        
-        this.dispatchEvent(
-            new ShowToastEvent(
-                    {
-                        title: 'Opp created successfully',
-                        message: 'Successfully created and record Id is ' + event.detail.id ,
-                        variant: 'success'
-                    }
-            )
-        );
-
+    showSuccess(event){
+        this.dispatchEvent(new ShowToastEvent({
+            title: 'Opp created successfully',
+            message: 'Successfully created an record Id is'+ event.detail.id,
+            variant: 'success'
+        }));
     }
-
 
 }
--- force-app/main/default/lwc/practice_24Edit/practice_24Edit.js
+++ force-app/main/default/lwc/practice_24Edit/practice_24Edit.js
@@ -1,4 +1,4 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 import OPP_OBJECT from "@salesforce/schema/Opportunity";
 import NAME_FIELD from "@salesforce/schema/Opportunity.Name";
 import STAGE_FIELD from "@salesforce/schema/Opportunity.StageName";
@@ -8,21 +8,28 @@ import AMOUNT_FIELD from "@salesforce/schema/Opportunity.Amount";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
-
-export default class Practice24 extends LightningElement {
+export default class Practice24_edit extends LightningElement {
 
     objectApiName = OPP_OBJECT; 
+    // if you are just planning to put this form on record page
+// you can even get the objectApiName directly from the record page without import @api objectAipName
+     //recordId='006Dn000006nNInIAM';
+     // this decoration is used to make this property public
+    // so the parent component or the record page can inject the recordId
+    // to this property automatically if we are on record page
+    @api
+    recordId;
 
     fields = [NAME_FIELD, STAGE_FIELD, CLOSE_DATE_FIELD, AMOUNT_FIELD]; 
 
 
-    showSuccess(event) {
-        
+    showSuccess() {
+
         this.dispatchEvent(
             new ShowToastEvent(
                     {
-                        title: 'Opp created successfully',
-                        message: 'Successfully created and record Id is ' + event.detail.id ,
+                        title: 'Opp Updated successfully',
+                        message: 'Successfully updated',
                         variant: 'success'
                     }
             )
