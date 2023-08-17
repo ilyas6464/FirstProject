ad998da839cfa4991cddc9ceeb2962bf60ed953f
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-08-17 11:40:22 -0400

    Joke Review

---
+++ force-app/main/default/lwc/reviewJoke/reviewJoke.js
@@ -0,0 +1,53 @@
+import { LightningElement, wire } from "lwc";
+
+import getJoke from "@salesforce/apex/ReviewJokeController.getJoke";
+
+export default class ReviewJoke extends LightningElement {
+
+    joke; // = "Something Funny";
+
+    // @wire(getJoke)
+    jokeFromApex; 
+
+    // @wire(getJoke)
+    // wireThisIntoFunction({ data,error}) {
+    //     if(data){
+    //          do something
+    //     } else if (error) {
+    //          do something else
+    //     }
+    // }
+    
+
+
+    handleClick() {
+        
+        console.log("Clicking Get Joke Button");
+        getJoke().then(result => {
+            console.log(result + '  : from Apex')
+            this.joke = result; 
+        } ); 
+
+    }
+
+    constructor() {
+        super(); 
+        console.log('CONSTRUCTOR RAN!!!');
+    }
+
+    connectedCallback() {
+        console.log('connectedCallback RAN!!!');
+        // this.joke = 'Super Fun from Apex'; 
+
+        // getJoke().then(result => {
+        //     console.log(result + '  : from Apex')
+        //     this.joke = result; 
+        // } ); 
+
+    }
+
+    renderedCallback() {
+        console.log('renderedCallback RAN!!!');
+    }
+
+}
