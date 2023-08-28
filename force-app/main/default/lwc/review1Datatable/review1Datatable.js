b78cb9b4c227516ebea8fdbbc4e212f68622047a
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-08-28 10:59:45 -0400

    review1Datatable

---
+++ force-app/main/default/lwc/review1Datatable/review1Datatable.js
@@ -0,0 +1,61 @@
+import getAccountWithAnnualRevenue from "@salesforce/apex/AccountController.getAccountWithAnnualRevenue";
+import { LightningElement, wire } from "lwc";
+
+const COLS = [
+    {
+        label: "Account Name",
+        fieldName: "recordLink",
+        type: "url",
+        typeAttributes: {
+          label: { fieldName: "Name" },
+          target: "_blank"
+        }
+      },
+    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' }
+];
+
+
+export default class Review1Datatable extends LightningElement {
+
+
+    accounts; // hold the wired data 
+    error; // hold error if there is any
+    columns = COLS; 
+
+    @wire(getAccountWithAnnualRevenue)
+    theDataFromApex({ data, error}){
+        
+        if (data) {
+            this.accounts = data.map(each => 
+                ({...each , recordLink: "/"+each.Id } )
+            ); 
+
+            this.error = undefined; 
+            console.log(data); 
+        } else if (error) {
+            this.data = undefined; 
+            this.error = error; 
+        }
+
+
+    }
+
+
+}
+
+
+/**
+
+[
+    {Name: 'GenePoint', AnnualRevenue: 30000000, Active__c: 'Yes'},
+    {Name: 'Edge Communications', AnnualRevenue: 139000000, Active__c: 'Yes'},
+    {Name: 'Burlington Textiles Corp of America', AnnualRevenue: 350000000, Active__c: 'Yes'}
+]
+
+[
+    {Name: 'ABC GenePoint', AnnualRevenue: 30000000, Active__c: 'Yes'},
+    {Name: 'ABC Edge Communications', AnnualRevenue: 139000000, Active__c: 'Yes'},
+    {Name: 'ABC Burlington Textiles Corp of America', AnnualRevenue: 350000000, Active__c: 'Yes'}
+]
+
+*/
