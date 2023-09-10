2992a1e1cd8ad455ae4e523c3877072d1a3780cf
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-09-10 14:29:04 -0400

    september 10

--- force-app/main/default/lwc/practice4/practice4.js
+++ force-app/main/default/lwc/practice4/practice4.js
@@ -12,12 +12,15 @@ export default class Practice4 extends LightningElement {
 
     handleNum1Change(event) {
         // set the value of num1 to what user typed
-        this.num1 =  parseFloat( event.target.value ) ; 
+        // turn String input into the number so we can take arithmetic operation
+        this.num1 = parseFloat(event.target.value); 
     }
 
     handleNum2Change(event) {
         // set the value of num2 to what user typed
-        this.num2 = parseFloat( event.target.value ); 
+        // turn String input into the number so we can take arithmetic operation
+        this.num2 = parseFloat(event.target.value); 
+        
     }
 
 
