0d1c8db3787267efcf7732d5873d47e8712b69db
ilyas6464 <118837315+ilyas6464@users.noreply.github.com>
2023-09-25 08:26:28 -0400

    updated

--- force-app/main/default/lwc/reviewExercise2/reviewExercise2.js
+++ force-app/main/default/lwc/reviewExercise2/reviewExercise2.js
@@ -2,6 +2,18 @@ import { LightningElement } from "lwc";
 export default class ReviewExercise2 extends LightningElement {
 
     isChecked = false;
+
+    get textToDisplay() {
+        
+        // if (this.isChecked) {
+        //     return "TEXT FOR CHECKED!!!";
+        // } else {
+        //     return "TEXT FOR UNCHECKED!!!"
+        // }
+        return (this.isChecked) ? "TEXT FOR CHECKED!!!" : "TEXT FOR UNCHECKED!!!"; 
+
+    }
+
     
     handleChange(event) {
         
